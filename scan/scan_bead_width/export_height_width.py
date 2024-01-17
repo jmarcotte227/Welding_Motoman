55c66c5844d6c06746b7601fa36abf8ec01b7126
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2024-01-16 21:09:19 -0500

    Worked on width-height data

---
+++ scan/scan_bead_width/export_height_width.py
@@ -0,0 +1,14 @@
+import numpy as np
+import sys
+import csv
+sys.path.append('../../weld')
+import weld_dh2v 
+import weld_w2v 
+
+with open('height_width_data.csv','w', newline='') as file:
+    writer=csv.writer(file)
+
+    for vel in range(5,16):
+        row = [weld_dh2v.v2dh_loglog(vel, 160, 'ER_4043'), weld_w2v.v2w_loglog(vel, 160, 'ER_4043')]
+        writer.writerow(row)
+        print(row)
