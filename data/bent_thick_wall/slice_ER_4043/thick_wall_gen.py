ed8a4ce951e36f6f4b429c33c5284175199ae736
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2024-01-31 21:29:42 -0500

    Generated Trajectories for welding

--- data/bent_thick_wall/slice_ER_4043/thick_wall_gen.py
+++ data/bent_thick_wall/slice_ER_4043/thick_wall_gen.py
@@ -47,7 +47,7 @@ def main():
     print('------------------------')
 
     #wall characteristics
-    wall_length = 100
+    wall_length = 100 
     wall_width = 30
     points_distance=0.5
     num_layers = 31
@@ -91,8 +91,9 @@ def main():
     vel_profile = [dh2v_loglog(min_dH, feed_speed, material)]
     height_profile = [min_dH]
     distance_of_height = [0]
-    #first segment
-    initial_layer[0:points_per_segment,0]=np.linspace(0,wall_width,points_per_segment)
+    #first segment\
+    initial_layer[0:points_per_segment,0]= np.ones(points_per_segment) * wall_length
+    initial_layer[0:points_per_segment,1]=np.linspace(0,wall_width,points_per_segment)
     initial_layer[0:points_per_segment,2]=vertical_shift
     initial_layer[0:points_per_segment,-1]=-np.ones(points_per_segment)
 
@@ -112,6 +113,7 @@ def main():
     ax_sim.plot(x_sim, bead_profile(h_curr, w_curr, x_sim, 0), color='b')
     idx = 1
     cum_distance = 0
+    dir_flag = True
 
 
     while(cum_distance<wall_length):
@@ -136,8 +138,11 @@ def main():
         
         print("Height Profile Height: ", prof_next_height)
         print("Bead height: ", h_next)
-        initial_layer[idx*points_per_segment:(idx+1)*points_per_segment,0]=np.linspace(wall_width, 0,points_per_segment)
-        initial_layer[idx*points_per_segment:(idx+1)*points_per_segment,1]=cum_distance
+        initial_layer[idx*points_per_segment:(idx+1)*points_per_segment,0]=wall_length-cum_distance
+        if dir_flag:
+            initial_layer[idx*points_per_segment:(idx+1)*points_per_segment,1]=np.linspace(wall_width, 0,points_per_segment)
+        else:
+            initial_layer[idx*points_per_segment:(idx+1)*points_per_segment,1]=np.linspace(0, wall_width,points_per_segment)
         initial_layer[idx*points_per_segment:(idx+1)*points_per_segment,2]=vertical_shift
         initial_layer[idx*points_per_segment:(idx+1)*points_per_segment,-1]=-np.ones(points_per_segment)
 
@@ -154,39 +159,50 @@ def main():
         distance, a, b, x_1, x_2, y_1, y_2 = overlap_distance(h_curr, w_curr, h_next, w_next)
         prof_next_height = height_profile_func(min_dH, max_dH, wall_length, cum_distance+distance)
 
-
+        dir_flag = not dir_flag
         idx = idx+1
 
-    plt.show()
+    #plt.show()
     
     
     fig, ax = plt.subplots(1,1)
     x = np.linspace(0, wall_length, 100)
     ax.plot(distance_of_height,height_profile)
     ax.plot(x, height_profile_func(min_dH, max_dH, wall_length, x))
-    plt.show()
+    #plt.show()
     
+    #eliminate empty elements of list
+    idx = 0
+    while(initial_layer[idx*points_per_segment,-1]):
+         idx+=1
+    end_idx = idx
+    initial_layer = initial_layer[:(end_idx)*points_per_segment, :]
+    
+    points_per_layer = end_idx*points_per_segment # update once connecting segments are interpolated
+    curve_curved=np.zeros((num_layers*points_per_layer,6))
+    curve_curved[:points_per_layer, :] = initial_layer[:,:]
+    for layer in range(num_layers-1):
+        for point in range(points_per_layer):
+              dx,dz = rotate([rot_point, 0], (curve_curved[layer*points_per_layer+point,0],curve_curved[layer*points_per_layer+point,2]),-layer_angle)
+              #print(dx,dz)
+              curve_curved[(layer+1)*points_per_layer+point,0] = dx
+              curve_curved[(layer+1)*points_per_layer+point,1] = curve_curved[point,1]
+              curve_curved[(layer+1)*points_per_layer+point,2] = dz
 
-    # for layer in range(num_layers-1):
-    #     for point in range(points_per_layer):
-    #           dx,dz = rotate([rot_point, 0], (curve_curved[layer*points_per_layer+point,0],curve_curved[layer*points_per_layer+point,2]),-layer_angle)
-    #           #print(dx,dz)
-    #           curve_curved[(layer+1)*points_per_layer+point,0] = dx
-    #           curve_curved[(layer+1)*points_per_layer+point,2] = dz
-
-    #           grav_dx,grav_dz = rotate((0,0), (curve_curved[layer*points_per_layer+point,3],curve_curved[layer*points_per_layer+point,5]),-layer_angle)
-    #           curve_curved[(layer+1)*points_per_layer+point,3] = grav_dx
-    #           curve_curved[(layer+1)*points_per_layer+point,5] = grav_dz
+              grav_dx,grav_dz = rotate((0,0), (curve_curved[layer*points_per_layer+point,3],curve_curved[layer*points_per_layer+point,5]),-layer_angle)
+              curve_curved[(layer+1)*points_per_layer+point,3] = grav_dx
+              curve_curved[(layer+1)*points_per_layer+point,5] = grav_dz
 
 
     vis_step=1
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    ax.plot3D(initial_layer[::vis_step,0],initial_layer[::vis_step,1],initial_layer[::vis_step,2],'r.-')
-    ax.quiver(initial_layer[::vis_step,0],initial_layer[::vis_step,1],initial_layer[::vis_step,2],initial_layer[::vis_step,3],initial_layer[::vis_step,4],initial_layer[::vis_step,5],length=10, normalize=True)
+    ax.plot3D(curve_curved[::vis_step,0],curve_curved[::vis_step,1],curve_curved[::vis_step,2],'r.-')
+    #ax.quiver(curve_curved[::vis_step,0],curve_curved[::vis_step,1],curve_curved[::vis_step,2],curve_curved[::vis_step,3],curve_curved[::vis_step,4],curve_curved[::vis_step,5],length=10, normalize=True)
+    ax.set_aspect('equal')
     plt.show()
 
-    #for layer in range(num_layers):
-	    #np.savetxt('slice_ER_4043/curve_sliced/slice'+str(layer+1)+'_0.csv',curve_curved[layer*points_per_layer:(layer+1)*points_per_layer],delimiter=',')
+    for layer in range(num_layers):
+	    np.savetxt('slice_ER_4043/curve_sliced/slice'+str(layer+1)+'_0.csv',curve_curved[layer*points_per_layer:(layer+1)*points_per_layer],delimiter=',')
         
 
 
