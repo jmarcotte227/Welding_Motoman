c638aae6348bb6c0fe20418298467b1ccb4ab58c
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2024-08-21 12:08:55 -0400

    Revising weld_benttube_correction

--- toolbox/angled_layers.py
+++ toolbox/angled_layers.py
@@ -4,6 +4,7 @@ using non-uniform height profiles.
 """
 
 import numpy as np
+import cv2
 
 
 def rotate(origin, point, angle):
@@ -20,6 +21,15 @@ def rotate(origin, point, angle):
     return qx, qy
 
 
+def delta_v(v):
+    """
+    returns the vector containing the differences between neighboring
+    elements.
+    """
+    delta_v_vec = [v[i] - v[i + 1] for i in range(len(v) - 1)]
+    return delta_v_vec
+
+
 def avg_by_line(labels, data_array, bins):
     """
     Averages the data in the variable "data" into the bins in "bins"
@@ -28,13 +38,13 @@ def avg_by_line(labels, data_array, bins):
     data_dim = data_array.shape[1]
 
     output = np.zeros((bins.shape[0], data_dim))
-    for idx,_bin in enumerate(bins):
-        avg_idxs = np.where(labels==_bin)[0]
+    for idx, _bin in enumerate(bins):
+        avg_idxs = np.where(labels == _bin)[0]
         if avg_idxs.shape[0] == 0:
-            output[idx,:] = [None]*data_dim
+            output[idx, :] = [None] * data_dim
         else:
-            total = np.sum(data_array[avg_idxs,:], axis=0)
-            output[idx,:] = total/avg_idxs.shape[0]
+            total = np.sum(data_array[avg_idxs, :], axis=0)
+            output[idx, :] = total / avg_idxs.shape[0]
     return output
 
 
@@ -69,9 +79,9 @@ class SpeedHeightModel:
         return v
 
     def model_update_rls(self, vels, dhs):
-        """updates the model coefficients using the recursive 
+        """updates the model coefficients using the recursive
         least-squares algorithm"""
-        # Algorithm from 
+        # Algorithm from
         # https://osquant.com/papers/recursive-least-squares-linear-regression/
         for idx, vel in enumerate(vels):
             x = np.array([[np.log(np.array(vel))], [1]])
@@ -82,3 +92,49 @@ class SpeedHeightModel:
                 e = y - x.T @ self.coeff_mat
                 self.coeff_mat = self.coeff_mat + k @ e
                 self.p = self.p / self.beta - k @ k.T * r
+
+
+def interpolate_heights(nom_height, sparse_height):
+    """
+    Fills in a sparse height profile with elements of the nominal height profile
+    """
+    for i, height in enumerate(sparse_height):
+        if np.isnan(height):
+            sparse_height[i] = nom_height[i]
+    return sparse_height
+
+
+def flame_detection_aluminum(
+    raw_img, threshold=1.0e4, area_threshold=4, percentage_threshold=0.8
+):
+    """
+    flame detection by raw counts thresholding and connected components labeling
+    centroids: x,y
+    bbox: x,y,w,h
+    adaptively increase the threshold to 60% of the maximum pixel value
+    """
+    threshold = max(threshold, percentage_threshold * np.max(raw_img))
+    thresholded_img = (raw_img > threshold).astype(np.uint8)
+
+    _, labels, stats, centroids = cv2.connectedComponentsWithStats(
+        thresholded_img, connectivity=4
+    )
+
+    valid_indices = np.where(stats[:, cv2.CC_STAT_AREA] > area_threshold)[0][
+        1:
+    ]  ###threshold connected area
+    if len(valid_indices) == 0:
+        return None, None, None, None
+
+    average_pixel_values = [
+        np.mean(raw_img[labels == label]) for label in valid_indices
+    ]  ###sorting
+    valid_index = valid_indices[
+        np.argmax(average_pixel_values)
+    ]  ###get the area with largest average brightness value
+
+    # Extract the centroid and bounding box of the largest component
+    centroid = centroids[valid_index]
+    bbox = stats[valid_index, :-1]
+
+    return centroid, bbox
