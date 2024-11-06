3ec6a5fbd44e18184b777d7cad920ddc4a4b2ec8
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2024-11-06 11:19:59 -0500

    Prepped toolpaths for hot and cold model tests on same geometry

--- redundancy_resolution/baseline_joint.py
+++ redundancy_resolution/baseline_joint.py
@@ -14,7 +14,7 @@ def main():
 		pulse2deg_file_path='../config/D500B_pulse2deg_real.csv',base_transformation_file='../config/D500B_pose.csv')
 	
 	dataset='bent_tube/'
-	sliced_alg='slice_ER_4043_small/'
+	sliced_alg='slice_ER_4043_large_cold/'
 	data_dir='../data/'+dataset+sliced_alg
 	with open(data_dir+'slicing.yml', 'r') as file:
 		slicing_meta = yaml.safe_load(file)
