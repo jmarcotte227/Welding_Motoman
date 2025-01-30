5d912de3a8fbdf89d860923da6e42bbee0a4dfcb
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2025-01-30 13:36:43 -0500

    working on ir data processing

--- FLIR/rr_process/flir_rr_tracking.py
+++ FLIR/rr_process/flir_rr_tracking.py
@@ -2,7 +2,9 @@ import RobotRaconteur as RR
 RRN=RR.RobotRaconteurNode.s
 import numpy as np
 from flir_toolbox import *
+from motoman_def import robot_obj, positioner_obj
 import inspect, traceback, os, sys
+import matplotlib.pyplot as plt
 sys.path.append("../../toolbox/")
 sys.path.append("")
 from angled_layers import flame_detection_aluminum
@@ -22,20 +24,21 @@ class FLIR_RR_TRACKING(object):
     def __init__(self, flir_service, robot_service):
 
         self.flir_service = flir_service
+        self.robot_service = robot_service
         self.ir_image_consts = RRN.GetConstants('com.robotraconteur.image', self.flir_service)
         self.cam_pipe=self.flir_service.frame_stream.Connect(-1)
-        self.cam_pipe.PacketRecievedEvent+=self.ir_cb
+        self.cam_pipe.PacketReceivedEvent+=self.ir_cb
         try:
             self.flir_service.start_streaming()
         except:
-            pass
+            print("unable to start streaming")
 
-        self.ir_process_struct=RRN.NewSturcutre("experimental.ir_process.ir_process_struct")
+        self.ir_process_struct=RRN.NewStructure("experimental.ir_process.ir_process_struct")
         self.flame_centroid_history = []
 
         ######## ROBOTS ########
         # Define Kinematics
-        CONFIG_DIR = '../config/'
+        CONFIG_DIR = '../../config/'
         self.robot=robot_obj(
             'MA2010_A0',
             def_path=CONFIG_DIR+'MA2010_A0_robot_default_config.yml',
@@ -57,6 +60,10 @@ class FLIR_RR_TRACKING(object):
             pulse2deg_file_path=CONFIG_DIR+'D500B_pulse2deg_real.csv',
             base_transformation_file=CONFIG_DIR+'D500B_pose.csv'
         )
+    # initialize display
+        self.fig, self.ax = plt.subplots()
+        self.ax.imshow(np.zeros((480,640)))
+        plt.show()
 
 
     def ir_cb(self,pipe_ep):
@@ -65,24 +72,25 @@ class FLIR_RR_TRACKING(object):
         '''
         while pipe_ep.Available > 0:
             # read the joint angles first
-            wire_val=robot_serivce.robot_state.PeekInValue()
-            q_cur=wire_val.joint_position
+            q_cur=self.robot_service.robot_state.PeekInValue()[0].joint_position
+            # print("q_cur: ", q_cur)
             # TODO: Check and see if this is actually quick enough to be accurate
 
             # read the image
-            rr_img = pipe_ep.RecievePacket()
-            if rr_img.image_invof.encoding == self.ir_image_consts["ImageEncoding"]["mono8"]:
+            rr_img = pipe_ep.ReceivePacket()
+
+            if rr_img.image_info.encoding == self.ir_image_consts["ImageEncoding"]["mono8"]:
                 mat = rr_img.data.reshape(
                     [rr_img.image_info.height, rr_img.image_info.width],
                     order='C'
                 )
-            elif rr_img.image_invof.encoding == self.ir_image_consts["ImageEncoding"]["mono16"]:
+            elif rr_img.image_info.encoding == self.ir_image_consts["ImageEncoding"]["mono16"]:
                 data_u16 = np.array(rr_img.data.view(np.uint16))
                 mat = data_u16.reshape(
                     [rr_img.image_info.height, rr_img.image_info.width],
                     order='C'
                 )
-            ir_format = rr_img.image_info.extend["ir_format"].data
+            ir_format = rr_img.image_info.extended["ir_format"].data
 
             if ir_format == "temperature_linear_10mK":
                 display_mat = (mat*0.01) - 273.15
@@ -92,7 +100,10 @@ class FLIR_RR_TRACKING(object):
                 display_mat = mat
 
             ir_image = np.rot90(display_mat, k=-1)
-            centroid, bbox = flame_detection_aluminum(ir_image)
+            self.ax.imshow(ir_image)
+            self.fig.canvas.draw()
+            centroid, _ = flame_detection_aluminum(ir_image)
+            print("centroid: ", centroid)
             if centroid is not None:
                 center_x = centroid[0]
                 center_y = centroid[1]
@@ -103,6 +114,7 @@ class FLIR_RR_TRACKING(object):
                 try:
                     self.ir_process_struct.flame_reading=int(flame_reading)
                     self.ir_process_struct.flame_position=centroid.astype(np.uint16)
+                    self.ir_process_result.OutValue=self.ir_process_struct
                 except:
                     traceback.print_exc()
 
