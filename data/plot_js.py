6ac6cfc467bcaeb23860bd65dd1615f3f4c3d3fe
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2025-08-08 16:34:39 -0400

    long wall data with appropriate waypoints

--- data/plot_js.py
+++ data/plot_js.py
@@ -6,33 +6,43 @@ import matplotlib.pyplot as plt
 
 
 def main():
-	dataset='s_curve_angled/'
-	sliced_alg='slice/'
-	data_dir='../data/'+dataset+sliced_alg
-	num_layers=106
-	num_baselayers=0
-	curve_sliced_js=[]
-	positioner_js=[]
-	for i in range(num_layers):
-		num_sections=len(glob.glob(data_dir+'curve_sliced_js/D500B_js'+str(i)+'_*.csv'))
-		for x in range(num_sections):
-			if i % 2 == 0:
-				# curve_sliced_js.append(np.loadtxt(data_dir+'curve_sliced_js/MA2010_js'+str(i)+'_'+str(x)+'.csv',delimiter=','))
-				positioner_js.append(np.loadtxt(data_dir+'curve_sliced_js/D500B_js'+str(i)+'_'+str(x)+'.csv',delimiter=','))
-			
-			else:
-				# curve_sliced_js.append(np.flip(np.loadtxt(data_dir+'curve_sliced_js/MA2010_js'+str(i)+'_'+str(x)+'.csv',delimiter=','),axis=0))
-				positioner_js.append(np.flip(np.loadtxt(data_dir+'curve_sliced_js/D500B_js'+str(i)+'_'+str(x)+'.csv',delimiter=','),axis=0))
+    dataset='wall/'
+    sliced_alg='1_5mm_slice/'
+    data_dir='../data/'+dataset+sliced_alg
+    num_layers=1
+    num_baselayers=0
+    num_sections = 1
+    curve_sliced_js=[]
+    positioner_js=[]
+    rob_js=[]
+    for i in range(num_layers):
+        # num_sections=len(glob.glob(data_dir+'curve_sliced_js/D500B_js'+str(i)+'_*.csv'))
+        for x in range(num_sections):
+            if i % 2 == 0:
+                # curve_sliced_js.append(np.loadtxt(data_dir+'curve_sliced_js/MA2010_js'+str(i)+'_'+str(x)+'.csv',delimiter=','))
+                positioner_js.append(np.loadtxt(data_dir+'curve_sliced_js/D500B_js'+str(i)+'_'+str(x)+'_forward.csv',delimiter=','))
+                rob_js.append(np.loadtxt(data_dir+'curve_sliced_js/MA2010_js'+str(i)+'_'+str(x)+'_forward.csv',delimiter=','))
+            
+            else:
+                # curve_sliced_js.append(np.flip(np.loadtxt(data_dir+'curve_sliced_js/MA2010_js'+str(i)+'_'+str(x)+'.csv',delimiter=','),axis=0))
+                positioner_js.append(np.flip(np.loadtxt(data_dir+'curve_sliced_js/D500B_js'+str(i)+'_'+str(x)+'_forward.csv',delimiter=','),axis=0))
+                rob_js.append(np.flip(np.loadtxt(data_dir+'curve_sliced_js/MA2010_js'+str(i)+'_'+str(x)+'_forward.csv',delimiter=','),axis=0))
 
-	# curve_sliced_js=np.concatenate( curve_sliced_js, axis=0)
-	positioner_js=np.concatenate( positioner_js, axis=0 )
+    # curve_sliced_js=np.concatenate( curve_sliced_js, axis=0)
+    positioner_js=np.concatenate( positioner_js, axis=0 )
+    rob_js=np.concatenate(rob_js, axis=0 )
+    print(rob_js.shape)
 
-	plt.plot(positioner_js,label=('q1','q2'))
-	plt.legend()
-	plt.title(' '+str(num_layers)+' slices')
-	plt.show()
+    plt.plot(positioner_js,label=('q1','q2'))
+    plt.legend()
+    plt.title(' '+str(num_layers)+' slices')
+    plt.show()
+    plt.plot(rob_js,label=('q1','q2','q3','q4', 'q5','q6'))
+    plt.legend()
+    plt.title(' '+str(num_layers)+' slices')
+    plt.show()
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- data/wall/traj_gen.py
+++ data/wall/traj_gen.py
@@ -1,30 +1,68 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
+import yaml
 
-total_height=100
-wall_length=100
-line_resolution=0.1
-points_distance=0.5
+total_height=75
+wall_length=50*np.pi
+base_length=wall_length+4*np.pi
+line_resolution=1.5
+base_resolution=4
+points_distance=np.pi
+y_position = 45
 num_layers=int(total_height/line_resolution)
+num_base = 2
 
+# generate layers
 points_per_layer=int(wall_length/points_distance)
 curve_dense=np.zeros((num_layers*points_per_layer,6))
 
 for layer in range(num_layers):
-	curve_dense[layer*points_per_layer:(layer+1)*points_per_layer,0]=np.linspace(0,wall_length,points_per_layer)
-	curve_dense[layer*points_per_layer:(layer+1)*points_per_layer,2]=layer*line_resolution
-print(curve_dense)
+	curve_dense[layer*points_per_layer:(layer+1)*points_per_layer,0]=np.linspace(0,wall_length,points_per_layer)-wall_length/2
+	curve_dense[layer*points_per_layer:(layer+1)*points_per_layer,1]=y_position
+	curve_dense[layer*points_per_layer:(layer+1)*points_per_layer,2]=layer*line_resolution+base_resolution*num_base
 
 curve_dense[:,-1]=-np.ones(len(curve_dense))
 
+for layer in range(num_layers):
+	np.savetxt('1_5mm_slice/curve_sliced_relative/slice'+str(layer)+'_0.csv',
+            curve_dense[layer*points_per_layer:(layer+1)*points_per_layer],
+            delimiter=',')
 
-vis_step=5
+# generate base
+points_per_base=int(base_length/points_distance)
+curve_base=np.zeros((num_base*points_per_base,6))
+
+for layer in range(num_base):
+	curve_base[layer*points_per_base:(layer+1)*points_per_base,0]=np.linspace(0,base_length,points_per_base)-base_length/2
+	curve_base[layer*points_per_base:(layer+1)*points_per_base,1]=y_position
+	curve_base[layer*points_per_base:(layer+1)*points_per_base,2]=layer*base_resolution
+
+curve_base[:,-1]=-np.ones(len(curve_base))
+
+for layer in range(num_layers):
+	np.savetxt('1_5mm_slice/curve_sliced_relative/baselayer'+str(layer)+'_0.csv',
+            curve_base[layer*points_per_base:(layer+1)*points_per_base],
+            delimiter=',')
+
+vis_step=1
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 ax.plot3D(curve_dense[::vis_step,0],curve_dense[::vis_step,1],curve_dense[::vis_step,2],'r.-')
-ax.quiver(curve_dense[::vis_step,0],curve_dense[::vis_step,1],curve_dense[::vis_step,2],curve_dense[::vis_step,3],curve_dense[::vis_step,4],curve_dense[::vis_step,5],length=1, normalize=True)
+ax.plot3D(curve_base[::vis_step,0],curve_base[::vis_step,1],curve_base[::vis_step,2],'b.-')
+# ax.quiver(curve_dense[::vis_step,0],curve_dense[::vis_step,1],curve_dense[::vis_step,2],curve_dense[::vis_step,3],curve_dense[::vis_step,4],curve_dense[::vis_step,5],length=1, normalize=True)
 plt.show()
 
-#for layer in range(num_layers):
-	#np.savetxt('dense_slice/curve_sliced/slice'+str(layer)+'_0.csv',curve_dense[layer*points_per_layer:(layer+1)*points_per_layer],delimiter=',')
+
+with open('1_5mm_slice/sliced_meta.yml', 'w') as file:
+    meta = {
+        'baselayer_length': points_per_base,
+        'baselayer_num': num_base,
+        'baselayer_resolution': base_resolution,
+        'layer_length': points_per_layer,
+        'layer_num': num_layers,
+        'layer_resolution': line_resolution,
+        'path_dl': points_distance
+    }
+    yaml.dump(meta,file)
+
 
