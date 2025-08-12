cf00fb990d11b2681c42758b4946d0b897c6f712
Jack Marcotte <jmarcotte227@gmail.com>
2025-08-12 10:37:24 -0400

    generating plots for CASE presentation

--- scan/angled_layer/model_fit/model_comp.py
+++ scan/angled_layer/model_fit/model_comp.py
@@ -7,7 +7,7 @@ plt.style.use('science')
 # plt.rcParams['text.usetex'] = True
 
 fig, ax1= plt.subplots(1,1)
-fig.set_size_inches(5,3)
+fig.set_size_inches(3,1.5)
 fig.set_dpi(300)
 
 vels=np.linspace(5,15,100)
@@ -21,14 +21,14 @@ f_hot=np.exp(a_hot*np.log(vels)+b_hot)
 ax1.plot(vels, f_cold)
 ax1.plot(vels, f_hot)
 ax1.grid()
-ax1.legend(["$\\bar{f}_{cold}$","$\\bar{f}_{hot}$"])
+# ax1.legend(["$\\bar{f}_{cold}$","$\\bar{f}_{hot}$"])
 ax1.legend(["$\\bar{f}_{cold}$","$\\bar{f}_{hot}$"],
            facecolor='white', 
            framealpha=0.8,
            frameon=True,)
 ax1.set_xlabel("$v_t$ (mm/s)")
 ax1.set_ylabel("$\Delta h$ (mm)")
-ax1.set_title("Model Comparison")
-plt.savefig("model_comparison.png")
+# ax1.set_title("Model Comparison")
+plt.savefig("model_comparison_pres.png", dpi=fig.dpi)
 plt.show()
 
