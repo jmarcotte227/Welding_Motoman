1025645f385f86cf85b8c59099b3d613b7b278b9
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2025-10-30 09:20:52 -0400

    img vis

--- FLIR/ir_vis_cv.py
+++ FLIR/ir_vis_cv.py
@@ -7,10 +7,10 @@ from angled_layers import *
 
 
 # Load the IR recording data from the pickle file
-with open('../../recorded_data/copper_leaktests_2025_09_05_16_51_50/layer_1_seg_1/ir_recording.pickle', 'rb') as file:
+with open('../../recorded_data/copper_leaktests_2025_09_12_13_11_43/layer_9_seg_0/ir_recording.pickle', 'rb') as file:
     ir_recording = pickle.load(file)
 
-ir_ts=np.loadtxt('../../recorded_data/copper_leaktests_2025_09_05_16_51_50/layer_1_seg_1/ir_stamps.csv', delimiter=',')
+ir_ts=np.loadtxt('../../recorded_data/copper_leaktests_2025_09_12_13_11_43/layer_9_seg_0/ir_stamps.csv', delimiter=',')
 
 print(len(ir_recording), len(ir_ts))
 
